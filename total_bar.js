1165175d449149d90f2eadf3b9ed01480b812a70
alex_tanti <atanti98@gmail.com>
2016-08-08 10:02:59 +0200

    Version 0.3
    Added cards and list container,
    Changed layout from 1 side content to 2 side content

---
+++ total_bar.js
@@ -0,0 +1,24 @@
+var bars = null;
+
+$(window).load(function setBars(){
+	bars = $("#explore_page .card_container .total_bar");
+	console.log("in");
+	console.log(bars);
+	$(bars).each(function(){
+		console.log("loop");
+		var yes_bar = $(this).find(".yes_bar");
+		var no_bar = $(this).find(".no_bar");
+		var yes_percent = $(yes_bar).attr("data-percent");
+		var no_percent = $(no_bar).attr("data-percent");
+		console.log(yes_percent+"  "+no_percent);
+		
+		$(yes_bar).attr("style","width:"+yes_percent+"%;");
+		$(no_bar).attr("style","width:"+no_percent+"%;");
+		
+		var yes_text = $(yes_bar).find("span");
+		var no_text = $(no_bar).find("span");
+		
+		$(yes_text).html(yes_percent+"%");
+		$(no_text).html(no_percent+"%");
+	});
+});
